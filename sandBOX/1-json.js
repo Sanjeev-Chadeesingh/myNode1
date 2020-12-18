7e3e9d61243c8060689c7889b47ce75876db002a
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-18 15:45:21 -0500

    json to js to json challenge

--- sandBOX/1-json.js
+++ sandBOX/1-json.js
@@ -9,7 +9,15 @@ const fs = require('fs');
 // fs.writeFileSync('1-json.json', bookJSON);
 
 //working with JSON!!
-const dataBuffer = fs.readFileSync('1-json.json');
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title);
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+
+const newData = fs.readFileSync('1-json.json');
+const readableData = newData.toString();
+const changeableData = JSON.parse(readableData);
+changeableData.name = 'Sanjeev';
+changeableData.age = 36;
+const newJSON = JSON.stringify(changeableData);
+fs.writeFileSync('1-json.json', newJSON);
