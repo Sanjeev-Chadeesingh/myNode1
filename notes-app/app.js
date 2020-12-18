591bec82b5f58a66af5db40be02bad110a8017c1
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-18 10:30:40 -0500

    First challenge, fs understanding, lets go!

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+//fs.writeFileSync('notes.txt', 'My name is Sanjeev');
+
+fs.appendFileSync('notes.txt', 'I am going to learn Node well enough to get a job and get my shit together!');
