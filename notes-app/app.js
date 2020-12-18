03ab99026f94e97b88d04ebd8a6ab62ded9dd3be
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-18 14:04:53 -0500

    yargs command challenge done!

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,47 @@
-const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-const command = process.argv[2];
+//Customiz yargs version
+yargs.version('1.1.0');
 
-console.log(process.argv);
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!');
+    }
+});
 
-if(command === 'add') {
-    console.log('Adding note');
-} else if(command === 'remove') {
-    console.log('Removing note');
-}
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing note',
+    handler: function() {
+        console.log('Removing the note!');
+    }
+});
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function() {
+        console.log('Listing notes!');
+    }
+});
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading notes',
+    handler: function() {
+        console.log('Reading notes!');
+    }
+});
+
+//add, remove, read, list NOTES
+
+
+console.log(yargs.argv);
