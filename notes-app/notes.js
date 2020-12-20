fed6bbe8d60bc8e63c4e1d002dcf3cbb3470b217
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-20 10:59:52 -0500

    saveNotes finished and added to addNotes, basetxt

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,7 +6,16 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes();
-    console.log(notes);
+    notes.push({
+        title,
+        body 
+    })
+   saveNotes(notes);
+}
+
+const saveNotes = notes => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
 const loadNotes = () => {
