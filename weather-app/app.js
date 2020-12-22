f9bd5c8963b06e3b0bea6d2d4d44746445ee9e67
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-22 12:59:22 -0500

    farenheit with query string

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233&units=f';
 
 request({ url, json: true }, (error, response) => {
     // console.log(response.body.current);
