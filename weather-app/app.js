47d88f5b194c2f4622742e3718585d3cee0e9eeb
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-22 12:52:50 -0500

    httpReq challenge complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233';
 
 request({ url, json: true }, (error, response) => {
-    console.log(response.body.current);
+    // console.log(response.body.current);
+    console.log(`It is currently ${response.body.current.temperature} degrees, but it feels like ${response.body.current.feelslike} degrees.`);
 });
