3858df7db7d582339968455d7872458002c44c06
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-22 11:02:41 -0500

    Async,nonBlock,cB queue,callstack, eventLoop basix

---
+++ weather-app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting...');
+
+setTimeout(() => {
+    console.log('Two Second Timer');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Zero Second Timer');
+}, 0);
+
+console.log('Stopping...');
