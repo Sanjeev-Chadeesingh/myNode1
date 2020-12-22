74edaa19438df2236d0d6da2eda06a1d00cfcdab
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-22 13:02:48 -0500

    finished weather with description at top

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d
 
 request({ url, json: true }, (error, response) => {
     // console.log(response.body.current);
-    console.log(`It is currently ${response.body.current.temperature} degrees, but it feels like ${response.body.current.feelslike} degrees.`);
+    console.log(`${response.body.current.weather_descriptions[0]}, and it's currently ${response.body.current.temperature} degrees, but it feels like ${response.body.current.feelslike} degrees.`);
 });
