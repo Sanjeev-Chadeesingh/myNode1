2c5bfd44ca6602ea9521204fa6097eb288c13d0c
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-26 11:26:13 -0500

    query made dynamic, function for geocode start

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,16 +12,29 @@ const request = require('postman-request');
 //     }
 // });
 
-const geocoderURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2owYjl1dGsxZjRvMndteXZ0dDA2djhoIn0.eRQ_alaM_Dt3ZFhCLxbbgg&limit=1';
+// const geocoderURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2owYjl1dGsxZjRvMndteXZ0dDA2djhoIn0.eRQ_alaM_Dt3ZFhCLxbbgg&limit=1';
 
-request({ url: geocoderURL, json: true }, (error, response) => {
-    if(error) {
-        console.log('Unable to connect to geocoder service.');
-    } else if(response.body.features.length === 0) {
-        console.log('Nothing seems to match your search. Check spelling / location and try again please.');
-    } else {
-        const lat = response.body.features[0].center[0];
-        const lon = response.body.features[0].center[1];
-        console.log(`Latitude: ${lat} + Longitude: ${lon}`);
-    }
-});
+// request({ url: geocoderURL, json: true }, (error, response) => {
+//     if(error) {
+//         console.log('Unable to connect to geocoder service.');
+//     } else if(response.body.features.length === 0) {
+//         console.log('Nothing seems to match your search. Check spelling / location and try again please.');
+//     } else {
+//         const lat = response.body.features[0].center[0];
+//         const lon = response.body.features[0].center[1];
+//         console.log(`Latitude: ${lat} + Longitude: ${lon}`);
+//     }
+// });
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2owYjl1dGsxZjRvMndteXZ0dDA2djhoIn0.eRQ_alaM_Dt3ZFhCLxbbgg&limit=1'
+    request({ url, json: true }, (error, response) => {
+        if(error) {
+            console.log()
+        }
+    })
+};
+
+geocode('Orlando', (error, data) => {
+
+})
