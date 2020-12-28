67c452a40dab7147486acee3abc53ed48130fd61
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-28 11:28:01 -0500

    initial skeleton w/basic request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(() => {
-    console.log('2 Second Timer Finished!!');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('0 Second Timer Finished!!');
-}, 0)
-
-console.log('Stopping');
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+})
