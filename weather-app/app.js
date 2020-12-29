7cd0b138afb73e874dd22f9a1d8c715d2754a8bf
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-29 00:26:49 -0500

    errorHandling for weatherstack

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,8 +3,14 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233&units=f';
 
 request({ url: url, json: true }, (error, response) => {
-    const data = response.body;
-    console.log(`${data.current.weather_descriptions}. It is currently ${data.current.temperature} degrees out. If feels like it's ${data.current.feelslike} degrees out.`);
+    if(error) {
+        console.log('Unable to connect to weather service.');
+    } else if(response.body.error) {
+        console.log('Unable to find location; check input.');
+    } else {
+        const data = response.body;
+        console.log(`${data.current.weather_descriptions}. It is currently ${data.current.temperature} degrees out. If feels like it's ${data.current.feelslike} degrees out.`);
+    }   
 });
 
 const urlTwo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Orlando.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2owYjl1dGsxZjRvMndteXZ0dDA2djhoIn0.eRQ_alaM_Dt3ZFhCLxbbgg&limit=1';
