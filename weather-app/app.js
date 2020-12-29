a23da269204ff5225075146e37bcfeca1706ab7a
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-29 00:11:50 -0500

    geocode call complete

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,12 @@ request({ url: url, json: true }, (error, response) => {
     const data = response.body;
     console.log(`${data.current.weather_descriptions}. It is currently ${data.current.temperature} degrees out. If feels like it's ${data.current.feelslike} degrees out.`);
 });
+
+const urlTwo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Orlando.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2owYjl1dGsxZjRvMndteXZ0dDA2djhoIn0.eRQ_alaM_Dt3ZFhCLxbbgg&limit=1';
+
+request({ url: urlTwo, json: true }, (error, response) => {
+    const data = response.body;
+    const longitude = data.features[0].center[0];
+    const latitude = data.features[0].center[1];
+    console.log(`Longitude: ${longitude}, Latitude: ${latitude}`);
+})
