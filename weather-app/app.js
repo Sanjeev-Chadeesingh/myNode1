a011b31a30cddd4e6f9a89896b0c5ddc1a7ebbe3
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-29 00:14:49 -0500

    latLon confusion sorted

--- weather-app/app.js
+++ weather-app/app.js
@@ -11,7 +11,7 @@ const urlTwo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Orlando.json?a
 
 request({ url: urlTwo, json: true }, (error, response) => {
     const data = response.body;
-    const longitude = data.features[0].center[0];
     const latitude = data.features[0].center[1];
-    console.log(`Longitude: ${longitude}, Latitude: ${latitude}`);
-})
+    const longitude = data.features[0].center[0];
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+});
