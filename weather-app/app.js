9b4769d93436bbe7f001d9a2dd09d5d2a7569098
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2021-01-05 10:35:36 -0500

    response parsed intoJSON with options obj

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233&units=f';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body);
 });
