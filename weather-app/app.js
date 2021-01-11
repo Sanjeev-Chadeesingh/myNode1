69717259444e887876b7ea6bb242b99e57c3bca5
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2021-01-11 15:22:42 -0500

    part1 of geocode w/dynamics

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('postman-request');
 const chalk = require('chalk');
+const { features } = require('process');
 
 const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d13dcb95d5bf7&query=37.8267,-122.4233&units=f';
 
@@ -30,8 +31,23 @@ const url = 'http://api.weatherstack.com/current?access_key=c0d42c0159b8f4878c0d
 
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiamVldmVzMDIxMCIsImEiOiJja2prN3BxNnYwNW52MnpwNG0xZDV5eDY4In0.PpIN8wuXpHPe_KV9qzgcHg&limit=1`;
+
+    request({ url, json: true }, (error, response) => {
+        if(error) {
+            callback('Unable to connect to location services', undefined);
+        } else if(response.body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined);
+        } else {
+            callback(undefined, {
+                longitude: response.body.features[0].center[0],
+                latitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            });
+        }
+    });
 };
 
 geocode('Orlando', (error, data) => {
-
+    console.log('Error', error);
+    console.log('Data', data);
 });
